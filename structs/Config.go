d9abc180438bdff39f18472126688c0f0d8f79d4
DeathVenom54 <fdragon602@gmail.com>
2022-01-31 11:09:49 +0530

    merge Config and Listener

--- structs/Config.go
+++ structs/Config.go
@@ -1,6 +1,19 @@
 package structs
 
 type Config struct {
-	Port      int16      `json:"port"`
+	Port      string     `json:"port"` // example: ":8080"
 	Listeners []Listener `json:"listeners"`
+}
+
+type Listener struct {
+	Name          string `json:"name"`
+	Endpoint      string `json:"endpoint"`
+	Directory     string `json:"directory"`
+	Command       string `json:"command"`
+	NotifyDiscord bool   `json:"notifyDiscord"`
+
+	Discord struct {
+		Webhook         string `json:"webhook"`
+		NotifyBeforeRun bool   `json:"notifyBeforeRun"`
+	}
 }
