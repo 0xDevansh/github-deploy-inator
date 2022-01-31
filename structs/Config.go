53c75104b893abd0e4aa61f3774389b0237471cd
DeathVenom54 <fdragon602@gmail.com>
2022-01-31 14:39:36 +0530

    add filters

--- structs/Config.go
+++ structs/Config.go
@@ -6,14 +6,22 @@ type Config struct {
 }
 
 type Listener struct {
-	Name          string `json:"name"`
-	Endpoint      string `json:"endpoint"`
-	Directory     string `json:"directory"`
-	Command       string `json:"command"`
-	NotifyDiscord bool   `json:"notifyDiscord"`
+	// required properties
+	Endpoint  string `json:"endpoint"`  // endpoint where http server will listen for hooks
+	Directory string `json:"directory"` // the directory in which the command will be run
+	Command   string `json:"command"`   // the command to run
+	// for your deployment, it is suggested to put the various commands
+	// in your scripts like in node.js or a .sh file, and execute it.
+
+	//additional filters
+	Branch  string   `json:"branch"`  // execute only if push is on this branch
+	Authors []string `json:"authors"` // execute only if head commit author is one of these
+
+	NotifyDiscord bool `json:"notifyDiscord"`
 
 	Discord struct {
 		Webhook         string `json:"webhook"`
 		NotifyBeforeRun bool   `json:"notifyBeforeRun"`
+		SendOutput      bool   `json:"sendOutput"`
 	}
 }
